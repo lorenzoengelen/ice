3504c82cad0fd63625dd3db3d08c8957c7746cfa
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-11-05 11:29:52 -0700

    index and app setup

---
+++ app/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Button } from 'react-bootstrap';
+
+const buttonInstance = (
+  <Button>Button Test</Button>
+);
+
+ReactDOM.render(buttonInstance, document.getElementById('app'));
